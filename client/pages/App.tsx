652515cb7cde7a3e26cb6483925b4e1cd517c807
leo-walton <leowaltondesign@gmail.com>
2025-12-21 12:19:11 +1300

    Finish basic summary window

--- client/pages/App.tsx
+++ client/pages/App.tsx
@@ -1,10 +1,12 @@
 import Home from './Home.tsx'
+import Welcome from './Welcome.tsx'
 
 function App() {
 
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Welcome />
     </>
   )
 }
